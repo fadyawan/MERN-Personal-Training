5b7d917078841496f6764699726315ba254cf5d4
fadyawan <muhammad.adyawan@kurniawan.co.uk>
2023-04-26 10:14:04 +0100

    some changes

--- express-app/index.js
+++ express-app/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('hi')
 }) 
 
-app.listen('3001')
+app.listen(port, () => console.log(`Listening on port ${port}`));
