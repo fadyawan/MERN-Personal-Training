fbf63b2d720ecd2124a3be4112722ab3608c3c4c
fadyawan <muhammad.adyawan@kurniawan.co.uk>
2023-05-04 10:40:49 +0100

    just a check in

--- react-app/src/Components/navBar/navBar.js
+++ react-app/src/Components/navBar/navBar.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import Button from '@mui/material/Button';
 import './navBar.css';
+import Button from '../button/Button';
+import logo from '../../logo.svg';
+import logoMobile from '../../logoMobile.svg';
+import { Toolbar, Typography } from '@material-ui/core';
 
 function NavBar() {
     return (
-        <div className='navBarButtons'>
-            <Button>Home</Button>
-            <Button>About</Button>
-            <Button>React</Button>
-            <Button>Express</Button>
-        </div>
+        <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className="bar">
+            <img src={logo} className="logo" alt="something"/>
+            <img src={logoMobile} className="logoMobile" alt="something"/>
+            <Typography variant="h6" className="menuItem">
+                About
+            </Typography>
+            <Typography variant="h6" className="menuItem">
+                Blog
+            </Typography>
+            <Typography variant="h6" className="menuItem">
+                Careers
+            </Typography>
+            <Typography variant="h6" className="menuItem">
+                Demos
+            </Typography>
+            <Typography variant="h6" className="menuItem">
+                Contact Us
+            </Typography>
+            <Button txt="Trial Our Product" />
+        </Toolbar>
     )
 }
 
-export default NavBar;
+export default NavBar
