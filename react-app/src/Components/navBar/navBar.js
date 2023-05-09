41748434c6699a02c010740912ab434392156a2b
fadyawan <muhammad.adyawan@kurniawan.co.uk>
2023-05-09 09:34:37 +0100

    Invalid hook call, trying to fix it

--- react-app/src/Components/navBar/navBar.js
+++ react-app/src/Components/navBar/navBar.js
@@ -1,33 +1,68 @@
 import React from 'react';
 import './navBar.css';
-import Button from '../button/Button';
+import Button from '../button/button';
 import logo from '../../logo.svg';
 import logoMobile from '../../logoMobile.svg';
 import { Toolbar, Typography } from '@material-ui/core';
+import {makeStyles} from "@material-ui/core/styles";
+
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor: "#fff",
+        ['@media (max-width:780px)']: { 
+           flexDirection: "column"
+          }
+    },
+    logo: {
+        width: "15%", 
+        ['@media (max-width:780px)']: { 
+           display: "none"
+           }
+    },
+    logoMobile:{
+        width: "100%", 
+        display: "none", 
+        ['@media (max-width:780px)']: { 
+            display: "inline-block"
+            }
+    },
+    menuItem: {
+        cursor: "pointer", 
+        flexGrow: 1,
+        "&:hover": {
+            color:  "#4f25c8"
+        },
+        ['@media (max-width:780px)']: { 
+            paddingBottom: "1rem"    }
+    }
+})
 
 function NavBar() {
+    const classes = styles()
     return (
-        <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className="bar">
-            <img src={logo} className="logo" alt="something"/>
-            <img src={logoMobile} className="logoMobile" alt="something"/>
-            <Typography variant="h6" className="menuItem">
-                About
-            </Typography>
-            <Typography variant="h6" className="menuItem">
-                Blog
-            </Typography>
-            <Typography variant="h6" className="menuItem">
-                Careers
-            </Typography>
-            <Typography variant="h6" className="menuItem">
-                Demos
-            </Typography>
-            <Typography variant="h6" className="menuItem">
-                Contact Us
-            </Typography>
-            <Button txt="Trial Our Product" />
-        </Toolbar>
+            <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
+                <img src={logo} className={classes.logo}/> 
+                <img src={logoMobile} className={classes.logoMobile}/> 
+                <Typography variant="h6" className={classes.menuItem}>
+                   About
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Blog
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Careers
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Demos 
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Contact Us 
+                </Typography>
+                <Button txt="Trial Our Product"/>
+            </Toolbar>
     )
 }
 
 export default NavBar
+
